6c49154415cadd709745281a58441e9c480b63eb
Shearerbeard <shearerbeard@gmail.com>
2015-11-13 18:06:06 -0700

    init commit

---
+++ img-batch/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to img-batch
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ img-batch/test/img_batch/core_test.clj
@@ -0,0 +1,7 @@
+(ns img-batch.core-test
+  (:require [clojure.test :refer :all]
+            [img-batch.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
