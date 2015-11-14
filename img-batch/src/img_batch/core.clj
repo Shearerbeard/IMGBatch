c12ba3fae4ee1f84757c9f424ef9edffec03f270
Shearerbeard <shearerbeard@gmail.com>
2015-11-13 18:09:52 -0700

    moving root dir

--- img-batch/src/img_batch/core.clj
+++
@@ -1,6 +0,0 @@
-(ns img-batch.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ src/img_batch/core.clj
@@ -0,0 +1,7 @@
+(ns img-batch.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
